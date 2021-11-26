82bc122eb1661f03030c2354556cb19c000ffe5c
Bulat <blutfullin@provectus.com>
2021-11-26 12:02:22 +0300

    refactor(reports): remove bivariate reports
    All related code is also removed to simplify the codebase.

--- drift_report/domain/statistical_report/statistical_feature_report.py
+++ drift_report/domain/statistical_report/statistical_feature_report.py
@@ -13,11 +13,9 @@ from drift_report.proto.monitoring_manager_pb2 import (
 )
 from drift_report.domain.data import NUMERICAL_DTYPES
 from scipy import stats
-from sklearn.preprocessing import KBinsDiscretizer, LabelEncoder
 
 from drift_report.domain.statistical_report.statistical_test import StatisticalTest
 from drift_report.domain.statistical_report.test_messages import (
-    mean_test_message,
     distribution_test_message,
     median_test_message,
     variance_test_message,
@@ -90,103 +88,6 @@ class FeatureReportFactory:
             return None
 
 
-class BivariateReportFactory:
-    @classmethod
-    def __encode_feature_report(cls, feature_report):
-        if isinstance(feature_report, NumericalFeatureReport):
-            # Transform numerical data into categorical
-            labels, training_data, production_data = cls.__discretize_numerical_report(
-                feature_report
-            )
-        elif isinstance(feature_report, CategoricalFeatureReport):
-            labels, training_data, production_data = cls.__encode_categorical_report(
-                feature_report
-            )
-        else:
-            raise NotImplementedError(f"type {type(feature_report)} not supported")
-
-        return labels, training_data, production_data
-
-    @classmethod
-    def get_feature_report(cls, feature_report_1, feature_report_2):
-
-        f1_labels, f1_training_data, f1_production_data = cls.__encode_feature_report(
-            feature_report_1
-        )
-        f2_labels, f2_training_data, f2_production_data = cls.__encode_feature_report(
-            feature_report_2
-        )
-
-        return BivariateFeatureReport(
-            feature_report_1.feature_name,
-            f1_labels,
-            f1_training_data,
-            f1_production_data,
-            feature_report_2.feature_name,
-            f2_labels,
-            f2_training_data,
-            f2_production_data,
-        )
-
-    @staticmethod
-    def __encode_categorical_report(feature_report: "CategoricalFeatureReport"):
-        """
-        use OrdinalEncoder to encode categories into ints
-
-        Parameters
-        ----------
-        feature_report
-
-        Returns
-        -------
-        tuple:
-        * List of labels used in human readable form e.g. ["Cat", "Dog", ..., "Frog"]
-        * Ordinally encoded training data
-        * Ordinally encoded production data
-        """
-
-        labels = feature_report.bins
-
-        encoder = LabelEncoder()
-        encoder.classes_ = np.array(labels)
-
-        training_data = encoder.transform(feature_report.training_data).flatten()
-        production_data = encoder.transform(feature_report.production_data).flatten()
-
-        return labels, training_data, production_data
-
-    @staticmethod
-    def __discretize_numerical_report(feature_report):
-        """
-        Returns
-        -------
-        tuple:
-
-        * List of labels used in human readable form e.g. ["<10", "10-15", ..., ">100"]
-        * Ordinally encoded binned training data
-        * Ordinally encoded binned production data
-        """
-        bin_edges = feature_report.bins
-
-        discretizer = KBinsDiscretizer(encode="ordinal", strategy="uniform")
-        discretizer.bin_edges_ = [np.array(bin_edges)]
-        discretizer.n_bins_ = np.array([len(bin_edges) - 1])
-
-        labels = np.array(
-            [f"{b1:.2f} <= {b2:.2f}" for b1, b2 in zip(bin_edges, bin_edges[1:])]
-        )
-
-        return (
-            labels,
-            discretizer.transform(
-                feature_report.training_data.reshape(-1, 1)
-            ).flatten(),
-            discretizer.transform(
-                feature_report.production_data.reshape(-1, 1)
-            ).flatten(),
-        )
-
-
 class StatisticalFeatureReport(ABC):
     def __init__(
         self, feature_name: str, training_data: np.array, production_data: np.array
@@ -205,15 +106,11 @@ class StatisticalFeatureReport(ABC):
         self.training_histogram_values = training_hist
         self.production_histogram_values = deployment_hist
 
-        self.bivariate_reports: List[BivariateFeatureReport] = []
-
     def __repr__(self):
         return f"Feature Report for {self.feature_name}"
 
     def process(self):
         logging.info(f"Calculating features for {self.feature_name}")
-        for bv in self.bivariate_reports:
-            bv.process()
 
     def to_json(self) -> Dict:
         return {
@@ -224,7 +121,6 @@ class StatisticalFeatureReport(ABC):
                 "training": self.training_histogram_values.tolist(),
             },
             "statistics": {test.name: test.as_json() for test in self.tests},
-            "bivariate_reports": [r.as_json() for r in self.bivariate_reports],
         }
 
     def get_warning(self) -> Optional[dict]:
@@ -236,22 +132,6 @@ class StatisticalFeatureReport(ABC):
                 "drift_probability_per_feature": self.drift_probability,
                 "message": f"The feature {self.feature_name} has drifted. Following statistics have changed: {changed_statistics}.",
             }
-
-    def combine(self, another: "StatisticalFeatureReport"):
-        """
-        Combine two statistical feature reports from features x1 and x2 respectively to calculate conditional
-        distribution X2|X1.
-
-        Parameters
-        ----------
-        another StatisticalFeatureReport
-
-        Returns
-        -------
-        """
-        self.bivariate_reports.append(
-            BivariateReportFactory.get_feature_report(self, another)
-        )
 
     @abstractmethod
     def _get_histogram(self) -> Tuple[np.array, np.array, np.array]:
@@ -277,7 +157,7 @@ class NumericalFeatureReport(StatisticalFeatureReport):
                 np.mean,
                 stats.ks_2samp,
                 distribution_test_message,
-                {'alternative': 'two-sided'},
+                {"alternative": "two-sided"},
             ),
             StatisticalTest(
                 "Median",
@@ -422,136 +302,3 @@ class CategoricalFeatureReport(StatisticalFeatureReport):
 
         self.is_processed = True
         self.drift_probability = np.mean([test.has_changed for test in self.tests])
-
-
-class HeatMapData:
-    def __init__(
-        self,
-        x_title: str,
-        y_title: str,
-        x_labels: np.array,
-        y_labels: np.array,
-        x: np.array,
-        y: np.array,
-    ):
-        """
-        Container for heatmap data to plot on the UI. Calculates densities between ordinally encoded labels
-        in x and y correspondingly
-        Parameters
-        ----------
-        x_title x axis name
-        y_title y axis name
-        x_labels  list of human readable x labels
-        y_labels  list of human readable y labels
-        x Ordinaly encoded x
-        y Ordinaly encoded y
-        """
-        self.x_title = x_title
-        self.y_title = y_title
-        self.x_labels = x_labels
-        self.y_labels = y_labels
-        self.x = x
-        self.y = y
-
-        intensity_list = []
-
-        # Computes heatmap density
-        for ordinal_label_y, _ in enumerate(y_labels):
-            for ordinal_label_x, _ in enumerate(x_labels):
-                x_mask = x == ordinal_label_x
-                if x_mask.sum() > 0:
-                    y_mask = y[x_mask] == ordinal_label_y
-                    intensity = np.round(y_mask.mean(), 3)
-                else:
-                    intensity = 0
-                intensity_list.append(intensity)
-
-        self.intensity = np.array(intensity_list).reshape(len(y_labels), len(x_labels))
-
-    def as_json(self):
-        return {
-            "x_axis_name": self.x_title,
-            "y_axis_name": self.y_title,
-            "x": self.x_labels.tolist(),
-            "y": self.y_labels.tolist(),
-            "density": self.intensity.tolist(),
-        }
-
-
-class BivariateFeatureReport:
-    def __init__(
-        self,
-        f1_name: str,
-        f1_labels: List[str],
-        training_f1_labels: np.array,
-        production_f1_labels: np.array,
-        f2_name: str,
-        f2_labels: List[str],
-        training_f2_labels: np.array,
-        production_f2_labels: np.array,
-    ):
-        """
-
-        Parameters
-        ----------
-        f1_name Name of a first feature
-        f1_labels List of human-readable labels for constructing a heatmap
-        training_f1_labels Ordinally encoded array of training labels
-        production_f1_labels Ordinally encoded array of production labels
-        f2_name
-        f2_labels
-        training_f2_labels
-        production_f2_labels
-        """
-        logging.info(f"Creating bivariate report between {f1_name} and {f2_name}")
-
-        self.f1_name = f1_name
-        self.f1_labels = f1_labels
-        self.training_f1_labels = training_f1_labels
-        self.production_f1_labels = production_f1_labels
-
-        self.f2_name = f2_name
-        self.f2_labels = f2_labels
-        self.training_f2_labels = training_f2_labels
-        self.production_f2_labels = production_f2_labels
-
-        # Calculate in self.process()
-        self.production_heatmap: HeatMapData = None
-        self.training_heatmap: HeatMapData = None
-
-        # Todo specify is ordinal or is categorical?!
-        # if ordinal-ordinal, then KS-test is used
-        # if categorical-categorical, then chisquare test is used
-        self.drifted: bool = False
-
-    def __repr__(self):
-        return f"Feature Report for {self.f1_name}|{self.f2_name}"
-
-    def process(self):
-        # TODO calculate GOF here?
-        self.production_heatmap = HeatMapData(
-            x_title=self.f1_name,
-            y_title=self.f2_name,
-            x_labels=self.f1_labels,
-            y_labels=self.f2_labels,
-            x=self.production_f1_labels,
-            y=self.production_f2_labels,
-        )
-
-        self.training_heatmap = HeatMapData(
-            x_title=self.f1_name,
-            y_title=self.f2_name,
-            x_labels=self.f1_labels,
-            y_labels=self.f2_labels,
-            x=self.training_f1_labels,
-            y=self.training_f2_labels,
-        )
-
-    def as_json(self):
-        return {
-            "feature_1": self.f1_name,
-            "feature_2": self.f2_name,
-            "drifted": self.drifted,
-            "training_heatmap": self.training_heatmap.as_json(),
-            "production_heatmap": self.production_heatmap.as_json(),
-        }
