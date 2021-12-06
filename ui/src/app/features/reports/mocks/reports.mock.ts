f6b3f0191dfe8af8fb2db04fcf32b169730713f1
Guzel Kafizova <66492070+Guzel738@users.noreply.github.com>
2021-12-06 16:47:19 +0300

    feat: add info about features to the reports page (#11)
    * feat: add info about features to the reports page
    
    * chore: simplify code
    
    * chore: refactoring
    
    Co-authored-by: Paul Mck <vixtir90@gmail.com>

--- ui/src/app/features/reports/mocks/reports.mock.ts
+++ ui/src/app/features/reports/mocks/reports.mock.ts
@@ -1,11 +1,17 @@
 import { Report } from '../models/reports';
-import { mockDrift } from '../../drift-report/mocks';
+import { mockDrift1, mockDrift2 } from '../../drift-report/mocks';
 
-export const mockReports: Report[] = [
+export const mockReports: Report[] | any = [
   {
     filename: 's3://adult/inference/batch_1.csv',
     model_name: 'adult',
     model_version: 1,
-    report: mockDrift,
+    report: mockDrift1,
+  },
+  {
+    filename: 's3://adult/inference/batch_2.csv',
+    model_name: 'adult',
+    model_version: 1,
+    report: mockDrift2,
   },
 ];
--- ui/src/app/features/reports/state/reports.query.ts
+++ ui/src/app/features/reports/state/reports.query.ts
@@ -6,6 +6,16 @@ import { RouterQuery } from '@datorama/akita-ng-router-store';
 import { map, pluck } from 'rxjs/operators';
 import { DriftReport } from '../../drift-report/models';
 
+interface PerFeatureReport {
+  [featureName: string]: { [x: string]: number };
+}
+
+interface ReportCommon {
+  report: {
+    per_feature_report: PerFeatureReport;
+  };
+}
+
 @Injectable()
 export class ReportsQuery extends Query<ReportsState> {
   reports$ = this.select((s) => s.reports);
@@ -13,6 +23,9 @@ export class ReportsQuery extends Query<ReportsState> {
   constructor(protected store: ReportsStore, private routerQuery: RouterQuery) {
     super(store);
   }
+
+  isFeatureFailed = (report: PerFeatureReport) => (feature: string) =>
+    report[feature]['drift-probability'] > 0.25;
 
   selectCurrentDriftReport(): Observable<DriftReport | undefined> {
     return combineQueries([
@@ -26,6 +39,29 @@ export class ReportsQuery extends Query<ReportsState> {
         );
       }),
       pluck('report')
+    );
+  }
+
+  selectReports() {
+    return this.reports$.pipe(
+      map(
+        (reports) =>
+          reports &&
+          reports.map((reportCommon: ReportCommon) => {
+            const perFeatureReport = reportCommon.report.per_feature_report;
+            const featureNames = Object.keys(perFeatureReport);
+
+            const failedFeatures = featureNames.filter(
+              this.isFeatureFailed(perFeatureReport)
+            );
+
+            return {
+              ...reportCommon,
+              failedFeatures,
+              featuresNumber: featureNames.length,
+            };
+          })
+      )
     );
   }
 }
