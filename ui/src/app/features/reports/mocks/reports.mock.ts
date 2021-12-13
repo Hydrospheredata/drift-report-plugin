d45e21d56a0ec963aa148e968e3d0327e9e91a6a
Guzel738 <guzel1625@gmail.com>
2021-12-13 13:36:26 +0300

    chore: change batches order from newest to oldest

--- ui/src/app/features/reports/mocks/reports.mock.ts
+++ ui/src/app/features/reports/mocks/reports.mock.ts
@@ -4,12 +4,21 @@ import { mockDrift1, mockDrift2 } from '../../drift-report/mocks';
 export const mockReports: Report[] | any = [
   {
     filename: 's3://adult/inference/batch_1.csv',
+    file_timestamp: '2021-12-10 15:41:24.298000',
     model_name: 'adult',
     model_version: 1,
     report: mockDrift1,
   },
   {
     filename: 's3://adult/inference/batch_2.csv',
+    file_timestamp: '2021-12-10 15:41:25.272000',
+    model_name: 'adult',
+    model_version: 1,
+    report: mockDrift2,
+  },
+  {
+    filename: 's3://adult/inference/batch_3.csv',
+    file_timestamp: '2021-12-10 15:41:24.786000',
     model_name: 'adult',
     model_version: 1,
     report: mockDrift2,
--- ui/src/app/features/reports/models/reports.ts
+++ ui/src/app/features/reports/models/reports.ts
@@ -2,6 +2,7 @@ import { DriftReport } from '../../drift-report/models';
 
 export interface Report {
   filename: string;
+  file_timestamp: string;
   model_name: string;
   model_version: number;
   report: DriftReport;
