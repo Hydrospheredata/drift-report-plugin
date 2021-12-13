b01f3c0234491a826f31a940d2c3623144738871
Guzel Kafizova <66492070+Guzel738@users.noreply.github.com>
2021-12-13 14:22:36 +0300

    chore: change batches order from newest to oldest (#17)

--- ui/src/app/features/reports/state/reports.service.ts
+++ ui/src/app/features/reports/state/reports.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ReportsHttpService } from '../reports-http.service';
 import { Report } from '../models/reports';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +14,14 @@ export class ReportsService {
     model_name: string;
     model_version: number;
   }): Observable<Report[]> {
-    return this.http.get<Report[]>(`report`, params);
+    return this.http.get<Report[]>(`report`, params).pipe(
+      map(reports =>
+        reports.sort((a, b) => {
+          return (
+            <any>new Date(a.file_timestamp) - <any>new Date(b.file_timestamp)
+          );
+        }),
+      ),
+    );
   }
 }
